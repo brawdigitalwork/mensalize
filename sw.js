5c43be29b4782f20183c5627f946b0a53a163bcd
brawdigitalwork <brawdigitalwork@gmail.com>
2026-05-25 15:14:13 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js — Service Worker Mensalize
 // Fase 5.1 — Atualização e Cache
-const APP_VERSION = '49';
+const APP_VERSION = '50.1';
 const CACHE_PREFIX = 'mensalize-';
 const CACHE_NAME = `${CACHE_PREFIX}${APP_VERSION}`;
 
